9487d7ab471ea647fc8fe74ee6d10749afb7b923
enculandus <68181342+enculandus@users.noreply.github.com>
2020-12-07 09:56:16 +0530

    Add files via upload

--- Portal/home.js
+++ Portal/home.js
@@ -1,13 +1,12 @@
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     // User is signed in.
-    window.alert("Already signed in");
-    var user = firebase.auth().currentUser;
+    //var user = firebase.auth().currentUser;
+    window.location.replace("login.html");
     if (user != null) {
 
     }
   } else {
     // No user is signed in.
-    window.alert("get out");
   }
 });
